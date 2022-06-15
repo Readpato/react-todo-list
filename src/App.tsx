3549a53d86c2e6e83003898c004fe9c502c3fc3b
Patrick Raedler <patoread@gmail.com>
2022-06-15 22:17:32 +0200

    feat: Try out react
    Trying out react for the first time

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,26 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type City = {
+  city: string;
+};
 
+let place: string = "world";
+// const MyButton = ({}) => <button>Change place</button>;
+const MyCityTitle = ({ city }: City) => <h2>A new place to see is {city}</h2>;
+
+function App({}) {
+  const [newCity, setNewCity] = useState("Paris");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>Hello {place}</h1>
+      <button onClick={() => setNewCity("Aachen")}>
+        Discover a new place!
+      </button>
+      {/* <MyButton /> */}
+      <MyCityTitle city={newCity} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
